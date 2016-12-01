d33cbbe8dc0177ccd7b8e0ab42e0d97680592820
Mike Elsmore <mike.elsmore@uk.ibm.com>
2016-12-01 11:51:24 +0000

    Add the delete tests for vertices

--- test/vertices.js
+++ test/vertices.js
@@ -23,18 +23,30 @@ var SERVER = url.format(parsed);
 
 // sample response
 var SAMPLE_RESPONSE = {
-  requestId: '71e9b56e-bded-402e-8fac-cfc83aec9c31',
+  requestId:'543f93b2-68e2-419b-9b5d-0b7e3289fd4f',
   status: {
-    message: '',
-    code: 200,
-    attributes: {},
+    message:'',
+    code:200,
+    attributes: {}
   },
   result: {
     data: [{
-      id: 1,
+      id:1,
+      label: 'vertex',
+      type: 'vertex',
+      properties: {
+        test:[{
+          id: '1cruoc-1crx40-9zzlx',
+          value: 'test'
+        }],
+        delete: [{
+          id: '1crv2k-1crx40-a8qv9',
+          value: 'delete'
+        }]
+      }
     }],
-    meta: {},
-  },
+    meta: {}
+  }
 };
 
 // load the GDS library and the http mocking library
@@ -80,6 +92,56 @@ describe('Vertices', function () {
         data.should.be.an.Object;
         mocks.done();
         done();
+      });
+    });
+
+  });
+
+  it('delete vertex', function (done) {
+    var mocks = nock(SERVER)
+                .post(PATH + '/vertices')
+                .reply(201, SAMPLE_RESPONSE)
+                .get(PATH + '/vertices/1')
+                .reply(200, SAMPLE_RESPONSE)
+                .delete(PATH + '/vertices/1')
+                .reply(200, [true]);
+
+    var g = new GDS({ url: APIURL, username: USERNAME, password: PASSWORD });
+
+    g.vertices().create({ test: 'test' }, function (err, data) {
+      var id = data.result.data[0].id;
+      g.vertices().get(id, function (err, data) {
+        g.vertices().delete(id, function (err, data) {
+          should(err).equal(null);
+          data.should.be.an.Object;
+          mocks.done();
+          done();
+        });
+      });
+    });
+
+  });
+
+  it('delete property vertex', function (done) {
+    var mocks = nock(SERVER)
+                .post(PATH + '/vertices')
+                .reply(201, SAMPLE_RESPONSE)
+                .get(PATH + '/vertices/1')
+                .reply(200, SAMPLE_RESPONSE)
+                .delete(PATH + '/vertices/1')
+                .reply(200, [true]);
+
+    var g = new GDS({ url: APIURL, username: USERNAME, password: PASSWORD });
+
+    g.vertices().create({ test: 'test', delete: 'delete' }, function (err, data) {
+      var id = data.result.data[0].id;
+      g.vertices().get(id, function (err, data) {
+        g.vertices().deleteProperty(id, {delete: 'delete'}, function (err, data) {
+          should(err).equal(null);
+          data.should.be.an.Object;
+          mocks.done();
+          done();
+        });
       });
     });
 
